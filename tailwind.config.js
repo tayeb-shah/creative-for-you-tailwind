277864104c4429ef1b2cff680587ba3c5d8309e1
tayeb-shah <shahtayeb404@gmail.com>
2024-12-13 23:39:51 +0600

    initial tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jakarta': ['"Plus Jakarta Sans", serif'],
+        'inter': ['"Inter", sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
